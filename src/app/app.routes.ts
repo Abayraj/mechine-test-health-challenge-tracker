b6e6903f48adb263defe928b87fc5090e127abfa
Abayraj <abayunni5@gmail.com>
2024-07-15 03:25:45 +0530

    feat: initial project setup with Tailwind CSS angular version 18

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { AdduserComponent } from '../pages/adduser/adduser.component';
+
+export const routes: Routes = [
+    { path: '', component: AdduserComponent },
+];
